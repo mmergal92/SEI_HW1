5bd3e36e0ae1bbc03b0ef8207b83f11d97afd1d8
mmergal92 <mmergal92@gmail.com>
2021-03-08 16:36:05 -0500

    Section 3: Maria

--- first_js_homework.js
+++ first_js_homework.js
@@ -52,3 +52,33 @@ console.log(g)
 
 //Bonus Challenge
 a < (b || f) === !f && e !== c 
+
+//Section 3
+
+//1. The first code that started with "while (true)" is and infinite loop. Since we're not defining a variable that will be false, the loop will keep running because that 'while' function will always be true.
+//2. I think this function will also be an infinite loop. Since runProgram is true as a constant, then when the while loop runs, it will continue to run as an infinite loop. The other possibility is that it could see that runProgram has 2 different values and send an error message instead and maybe not run it at all. 
+
+//let letters = "A";
+//let i = 0;
+
+// start a while loop that will run as log as 'i' is less than 20
+//while (i < 20) {
+// This will print the letters of the alphabet by adding 1 letter each times it runs. 
+//letters += "A";
+//i++;
+//}
+// Since "i" is going up by 1 and it's adding 1 letter to A, I think it will print letters A throught T.
+
+//console.log(letters);
+//The console log will print letters A throught T and then stop since the while loop will be false after the 20th letter.
+
+let letters = "A";
+let i = 0;
+
+while (i < 20) {
+	letters += "A";
+	i++;
+}
+
+console.log(letters);
+//The code was not what I expected. It ran 20 letter A's instead of the letters A-T (first 20 letter of the alphabet). I realized it's add A to A everytime and not actually changing the letter since there is no command written for that in this code. Therefore, it is 20 A's in a row.
